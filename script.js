f4326c8791be2aef8286867c32325327168e87a2
makara48 <113087636+makara48@users.noreply.github.com>
2024-11-25 14:39:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function bulboff(){
+    let i = document.getElementById('img')
+    i.src = "Off.jpg";
+}
+
+function bulbon(){
+    let i = document.getElementById('img')
+    i.src = "On.jpg";
+}
